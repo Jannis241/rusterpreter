40b297382126e0bcf6a27809237a708a680e2fad
Jannis241 <maus@sleepy.home>
2024-07-26 21:52:22 +0200

    sdfhj

--- src/file_reader.rs
+++ src/file_reader.rs
@@ -1,3 +1,38 @@
+use std::{env, process::exit};
+use std::fs::{self, read};
+use std::io::Error;
+
+
+fn get_file_name() -> String{
+    let mut file_name: Vec<String> = env::args().collect();
+    file_name.remove(0);
+
+    file_name.get(0).unwrap_or(&"main".to_string()).clone()
+
+}
+
+fn read_file(filename: String) -> String{
+    let content = fs::read_to_string(filename);
+
+    match content {
+        Ok(content) => content,
+        Err(e) => {
+            println!("DD: file not found");
+            exit(69)
+        }
+    }
+}
+
+
 pub fn get_input() -> String {
-    String::new()
+    let file_name = get_file_name();
+
+    let file_name = if file_name.ends_with(".dd") {
+        file_name.to_string()
+    } else {
+        format!("{}.dd", file_name)
+    };
+    let content = read_file(file_name);
+
+    return content;
 }
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,9 @@ use crate::lexer::Lexer;
 
 
 
-
-
-
 fn main() {
+                        
+
     let input = file_reader::get_input();
     let tokens = Lexer::create_tokens(input.into());
     println!("{:?}", tokens)
